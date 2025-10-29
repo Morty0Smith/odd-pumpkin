42d0e6bc1c3bc1b42b0bf7270e86f80af693041c
MortySmith <63672127+Morty0Smith@users.noreply.github.com>
2025-10-29 19:44:29 +0100

    Marged

--- scripts/enimy.gd
+++ scripts/enimy.gd
@@ -3,9 +3,14 @@ extends Node2D
 @export var movement:EnimyMovement
 @export var viewcone:Area2D
 @export var playerCollision:CollisionObject2D
+@export var characterBody:CharacterBody2D
+@export var gravity_component: GravityComponent
 
 func _physics_process(delta: float) -> void:
-	movement.moveNormalCycle()
+	gravity_component.handle_gravity(characterBody,delta)
 	var canSeePlayer:bool = playerCollision in viewcone.get_overlapping_bodies()
 	if (canSeePlayer):
 		movement.goToPos(playerCollision.position)
+	else:
+		movement.moveNormalCycle()
+	
